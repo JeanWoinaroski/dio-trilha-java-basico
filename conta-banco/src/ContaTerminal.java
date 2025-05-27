2a3e64b919c1334d7a00dbdeb011c1c20183e2a2
JeanWoinaroski <jeanwoinaroski@gmail.com>
2025-05-27 14:20:17 -0300

    alteraçoes conta terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TOD:Conhecer e importar as classes Scanner e Conta
+        //Exibir as mensagens para nosso usuario
+        //obter pela classe Scanner os dados do usuario
+        //Exibir a mensagem conta criada com sucesso
+
+        // Declaração das variáveis
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+        // Exibir mensagem de boas-vindas
+        System.out.println("===================================");
+        System.out.println("          BANCO JWoinaroski              ");
+        System.out.println("===================================");
+        System.out.println("Bem-vindo ao Banco Jwoinaroski!");
+        // Criar um objeto Scanner para ler a entrada do usuário
+        Scanner scanner = new Scanner(System.in);
+        // Ler o número da conta-
+        System.out.println("Por favor, digite o número da conta:");
+        numero = scanner.nextInt();
+        System.out.println("Por favor, digite o número da agência:");
+        // Ler o número da agência
+        agencia = scanner.next();
+        System.out.println("Por favor, digite o nome do cliente:");
+        // Limpar o buffer do scanner
+        scanner.nextLine();
+        // Ler o nome do cliente
+        nomeCliente = scanner.nextLine();
+        System.out.println("Por favor, digite o saldo da conta:");
+        // Ler o saldo da conta
+        saldo = scanner.nextDouble();
+        // Exibir a mensagem de conta criada com sucesso
+        System.out.println("Conta criada com sucesso!");
+        System.out.println("Número da conta: " + numero);
+        System.out.println("Agência: " + agencia);
+        System.out.println("Nome do cliente: " + nomeCliente);
+        System.out.println("Saldo: R$ " + saldo);
+        // Fechar o scanner
+        scanner.close();
+        // Exibir mensagem de encerramento
+        System.out.println("Obrigado por usar o nosso Banco Jwoinaroski ! " + nomeCliente);
+        System.out.println("===================================");
+    
     }
 }
